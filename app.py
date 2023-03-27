ab9cdf684408d40e6d3ccab93077397eafbaa47f
Recep Suluker <95488902+recepsuluker@users.noreply.github.com>
2023-03-27 10:53:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ ser.reset_input_buffer()
 
 @app.route("/")
 def home():
-    return render_template('clm.html')
+    return render_template('index.html')
 @app.route("/get_values")
 def get_values():
     #this is PWM, current value for
